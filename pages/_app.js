fe67d9faea37cc932e4a6b50a3dbdab539290119
Deepak Mandal <deepak379mandal@gmail.com>
2023-06-12 22:22:38 +0530

    Added UserProvider in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { StoreProvider } from '@/utils/Store'
+import UserProvider from '@/utils/providers/user.provider';
 import { useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,8 +9,10 @@ export default function App({ Component, pageProps }) {
 
   return (
     <StoreProvider>
-      <ToastContainer />
-      <Component {...pageProps} />
+      <UserProvider>
+        <ToastContainer />
+        <Component {...pageProps} />
+      </UserProvider>
     </StoreProvider>
   )
 }
