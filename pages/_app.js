f5ea4351467bc4d2ed1374b31c9423893cb7291e
Deepak Mandal <deepak379mandal@gmail.com>
2023-06-12 19:43:22 +0530

    Completed with testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '@/styles/globals.css'
 import { StoreProvider } from '@/utils/Store'
+import { useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps }) {
+
   return (
-  <StoreProvider>
-    <Component {...pageProps} />
-  </StoreProvider>
+    <StoreProvider>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </StoreProvider>
   )
 }
